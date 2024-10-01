b0cfde9a67e2170482eeb9168a7523a298c23ed7
Jesse Chong <jessechong@pursuit.org>
2024-10-01 13:07:00 -0400

    feat(Privacy Policy): :sparkles: Added a privacy-policy to the footer
    PrivacyPolicy is used to get access to the linkedin API.

--- src/components/shared/AppFooterCopyright.jsx
+++ src/components/shared/AppFooterCopyright.jsx
@@ -1,24 +1,28 @@
 function AppFooterCopyright() {
-	return (
-		<div className="font-general-regular flex justify-center items-center text-center">
-			<div className="text-lg text-ternary-dark dark:text-ternary-light">
-				&copy; {new Date().getFullYear()}
-				<a
-					href="https://github.com/Jesse-Chong"
-					target="__blank"
-					className="hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
-				>
-					React & Tailwind CSS Portfolio
-				</a>
-				.
-				<p
-					className="text-secondary-dark dark:text-secondary-light font-medium uppercase hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
-				>
-					Jesse
-				</p>
-			</div>
-		</div>
-	);
+  return (
+    <div className="font-general-regular flex justify-center items-center text-center">
+      <div className="text-lg text-ternary-dark dark:text-ternary-light">
+        &copy; {new Date().getFullYear()}
+        <a
+          href="https://github.com/Jesse-Chong"
+          target="__blank"
+          className="hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
+        >
+          React & Tailwind CSS Portfolio
+        </a>
+        .
+        <p className="text-secondary-dark dark:text-secondary-light font-medium uppercase hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500">
+          Jesse
+        </p>
+        <a
+          href="/privacy-policy"
+          className="hover:underline text-secondary-dark dark:text-secondary-light hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
+        >
+          Privacy Policy
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default AppFooterCopyright;
